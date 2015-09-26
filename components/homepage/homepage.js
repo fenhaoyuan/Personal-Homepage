aa7a826ff30b98b61da6f9bf398c69cabf7f34f4
Stephen Yuan <fenhaoyuan@gmail.com>
2015-09-27 09:51:58 +1000

    Re-written weather section
    Changed weather API.
    Changed layout and style.

--- components/homepage/homepage.js
+++ components/homepage/homepage.js
@@ -2,55 +2,20 @@
 
 var homepage = angular.module('homepage', []);
 
-homepage.controller('homepageCtrl', ['$scope',
-    function($scope) {
-        $scope.favourites = [
-            {
-                title: '6park',
-                href: 'http://www.6park.com',
-                img: 'http://www.6park.com/favicon.ico'
-            },
-            {
-                title: 'Google',
-                href: 'http://www.google.com.au',
-                img: 'http://www.google.com.au/favicon.ico'
-            },
-            {
-                title: 'Netbank',
-                href: 'https://www.my.commbank.com.au/netbank',
-                img: 'https://www.commbank.com.au/favicon.ico'
-            },
-            {
-                title: 'Westpac',
-                href: 'http://www.westpac.com.au/netbank',
-                img: 'http://www.westpac.com.au/favicon.ico'
-            },
-            {
-                title: 'Outlook',
-                href: 'http://www.outlook.com',
-                img: 'http://www.outlook.com/favicon.ico'
-            },
-            {
-                title: 'Facebook',
-                href: 'http://www.facebook.com',
-                img: 'https://fbstatic-a.akamaihd.net/rsrc.php/yl/r/H3nktOa7ZMg.ico'
-            },
-            {
-                title: 'dytt8',
-                href: 'http://www.dytt8.net/',
-                img: 'http://www.dytt8.net/favicon.ico'
-            },
-            {
-                title: 'flybuys',
-                href: 'https://www.flybuys.com.au',
-                img: 'https://www.flybuys.com.au/opencms/opencms/flybuys-static/imgs/root/favicon.png'
-            },
-            {
-                title: 'Maps',
-                href: 'https://www.google.com.au/maps',
-                img: 'https://www.google.com/images/branding/product/ico/maps_32dp.ico'
-            }
-        ];
+homepage.controller('homepageCtrl', ['$scope', 'favourites', '$http', 'weatherAPI',
+    function($scope, favourites, $http, weatherAPI) {
+        $scope.favourites = favourites;
+        
+        $scope.forecast = function() {
+            $http.get(weatherAPI.Melbourne).then(
+                function(response) {
+                    if (response.data.response.features.forecast) {
+                        $scope.weatherForecast = response.data.forecast.simpleforecast.forecastday;
+                    }
+                }
+            );
+        };
+        $scope.forecast();
     }
 ]);
 
@@ -67,11 +32,8 @@ homepage.directive('favourite', [
     function() {
         return {
             templateUrl: 'favourite.d.html',
-            controller: ['$scope', '$window',
-                function($scope, $window) {
-                    $scope.direct = function() {
-                        $window.location.href = $scope.website.href;
-                    };
+            controller: ['$scope',
+                function($scope) {
                 }
             ],
             scope: {
@@ -81,67 +43,69 @@ homepage.directive('favourite', [
     }
 ]);
 
-homepage.directive('weather', [
-    function() {
-        return {
-            templateUrl: 'weather.d.html',
-            controller: ['$scope', '$http',
-                function($scope, $http) {
-                    $scope.current = function() {
-                        var url = 'http://api.openweathermap.org/data/2.5/weather?id=7839805&units=metric';
-                        $http.get(url).then(
-                            function(response) {
-//                                console.log(response.data);
-                                $scope.weatherCurrent = response.data;
-                            }
-                        );
-                    };
-                    
-                    $scope.forecast = function() {
-                        var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?id=7839805&units=metric';
-                        $http.get(url).then(
-                            function(response) {
-                                console.log(response.data);
-                                $scope.weatherForecast = response.data;                                
-                            }
-                        );
-                    };
-                    
-                    $scope.current();
-                    $scope.forecast();
-                    
-                }
-            ],
-            scope: {
-            }
-        }
-    }
-]);
-
 homepage.directive('weatherDay', [
     function() {
         return {
             templateUrl: 'weather-day.d.html',
             controller: ['$scope',
                 function($scope) {
-                    if (angular.isDefined($scope.isForecast) && $scope.isForecast) {
-                        var date = new Date($scope.day.dt*1000);
-                        $scope.date = date.getDate() + '/' + (date.getMonth() + 1);
-                        $scope.tempMin = $scope.day.temp.min;
-                        $scope.tempMax = $scope.day.temp.max;
-                    } else {
-                        $scope.tempCurrent = $scope.day.main.temp;
-                        $scope.tempMin = $scope.day.main.temp_min;
-                        $scope.tempMax = $scope.day.main.temp_max;
-                    }
-                    $scope.description = $scope.day.weather[0].description;
-                    $scope.icon = 'http://openweathermap.org/img/w/' + $scope.day.weather[0].icon + '.png';
                 }
             ],
             scope: {
-                day: '=',
-                isForecast: '='
+                day: '='
             }
         };
     }
 ]);
+
+homepage.value('favourites', [
+    {
+        title: '6park',
+        href: 'http://www.6park.com',
+        img: 'http://www.6park.com/favicon.ico'
+    },
+    {
+        title: 'Google',
+        href: 'http://www.google.com.au',
+        img: 'http://www.google.com.au/favicon.ico'
+    },
+    {
+        title: 'Netbank',
+        href: 'https://www.my.commbank.com.au/netbank',
+        img: 'https://www.commbank.com.au/favicon.ico'
+    },
+    {
+        title: 'Westpac',
+        href: 'http://www.westpac.com.au/netbank',
+        img: 'http://www.westpac.com.au/favicon.ico'
+    },
+    {
+        title: 'Outlook',
+        href: 'http://www.outlook.com',
+        img: 'http://www.outlook.com/favicon.ico'
+    },
+    {
+        title: 'Facebook',
+        href: 'http://www.facebook.com',
+        img: 'https://fbstatic-a.akamaihd.net/rsrc.php/yl/r/H3nktOa7ZMg.ico'
+    },
+    {
+        title: 'dytt8',
+        href: 'http://www.dytt8.net/',
+        img: 'http://www.dytt8.net/favicon.ico'
+    },
+    {
+        title: 'flybuys',
+        href: 'https://www.flybuys.com.au',
+        img: 'https://www.flybuys.com.au/opencms/opencms/flybuys-static/imgs/root/favicon.png'
+    },
+    {
+        title: 'Maps',
+        href: 'https://www.google.com.au/maps',
+        img: 'https://www.google.com/images/branding/product/ico/maps_32dp.ico'
+    }
+]);
+
+homepage.value('weatherAPI', {
+    Melbourne: 'http://api.wunderground.com/api/4e91f18809720bd8/forecast/q/Australia/Melbourne.json'
+});
